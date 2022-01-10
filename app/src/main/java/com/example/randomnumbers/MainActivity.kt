29dfb75c99e9c0c6a828de9d5f700771b295d054
Gabriel Paganini <gabrielpaganinibarroso@hotmail.com>
2022-01-10 18:41:18 -0300

    adicionado novo background color e metodo de geração de números randomicos

--- app/src/main/java/com/example/randomnumbers/MainActivity.kt
+++ app/src/main/java/com/example/randomnumbers/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.randomnumbers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        randomButton.setOnClickListener(this)
+        textNumber.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.randomButton || v?.id == R.id.textNumber) {
+            val id = random()
+        }
+    }
+
+    fun random() : Int {
+        return Random.nextInt(1, 50)
     }
 }
